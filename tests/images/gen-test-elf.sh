29d8d5d7122dceb341d093b65e0f1e2c491196ec
Logan Chien <tzuhsiang.chien@gmail.com>
2011-06-03 09:53:39 +0800

    Update test case for x86_32 and x86_64.

--- tests/images/gen-test-elf.sh
+++ tests/images/gen-test-elf.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-clang -integrated-as -c test.c -o test-arm.o -ccc-host-triple armv7-none-linux-gnueabi -std=c89
-clang -integrated-as -c simple-test.c -o simple-test-arm.o -ccc-host-triple armv7-none-linux-gnueabi -std=c89
-clang -integrated-as -c test.c -o test-x86_32.o -ccc-host-triple i686-none-linux -std=c89
-clang -integrated-as -c test.c -o test-x86_64.o -ccc-host-triple x86_64-none-linux -std=c89
+clang -integrated-as -std=c89 -c test.c -o test-arm.o -ccc-host-triple armv7-none-linux-gnueabi
+clang -integrated-as -std=c89 -c test.c -o test-x86_32.o -ccc-host-triple i686-none-linux
+clang -integrated-as -std=c89 -c test.c -o test-x86_64.o -ccc-host-triple x86_64-none-linux
+
+clang -integrated-as -std=c89 -c simple-test.c -o simple-test-arm.o -ccc-host-triple armv7-none-linux-gnueabi
+clang -integrated-as -std=c89 -c simple-test.c -o simple-test-x86_32.o -ccc-host-triple i686-none-linux
+clang -integrated-as -std=c89 -c simple-test.c -o simple-test-x86_64.o -ccc-host-triple x86_64-none-linux
