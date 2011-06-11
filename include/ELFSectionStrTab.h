8e233aace27dd4c658db39ddd223bdc21121a4f2
Logan Chien <tzuhsiang.chien@gmail.com>
2011-06-11 17:04:49 +0800

    Split ELFSectionStrTab.h into declaration and implementation.

--- include/ELFSectionStrTab.h
+++ include/ELFSectionStrTab.h
@@ -3,14 +3,6 @@
 
 #include "ELFSection.h"
 
-#include "utils/helper.h"
-#include "utils/serialize.h"
-#include "utils/raw_ostream.h"
-
-#include <llvm/ADT/OwningPtr.h>
-#include <llvm/Support/Format.h>
-#include <llvm/Support/raw_ostream.h>
-
 #include <vector>
 
 template <unsigned Bitwidth> class ELFSectionHeader;
@@ -27,43 +19,15 @@ private:
 public:
   template <typename Archiver>
   static ELFSectionStrTab *
-  read(Archiver &AR, ELFSectionHeader<Bitwidth> const *sh) {
-    llvm::OwningPtr<ELFSectionStrTab> st(new ELFSectionStrTab());
-    st->buf.resize(sh->getSize());
+  read(Archiver &AR, ELFSectionHeader<Bitwidth> const *sh);
 
-    // Save section_header
-    st->section_header = sh;
-
-    AR.seek(sh->getOffset(), true);
-    AR.prologue(sh->getSize());
-    AR.read_bytes(&*st->buf.begin(), sh->getSize());
-    AR.epilogue(sh->getSize());
-
-    if (!AR) {
-      // Unable to read the string table.
-      return 0;
-    }
-
-    return st.take();
-  }
-
-  virtual void print() const {
-    using namespace llvm;
-
-    out() << '\n' << fillformat('=', 79) << '\n';
-    out().changeColor(raw_ostream::WHITE, true);
-    out() << "ELF String Table: " << this->section_header->getName() << '\n';
-    out().resetColor();
-    out() << fillformat('-', 79) << '\n';
-
-    dump_hex((unsigned char const *)&*buf.begin(), buf.size(), 0, buf.size());
-
-    out() << fillformat('=', 79) << '\n';
-  }
+  virtual void print() const;
 
   char const *operator[](size_t index) const {
     return &*buf.begin() + index;
   }
 };
+
+#include "impl/ELFSectionStrTab.hxx"
 
 #endif // ELF_SECTION_STRTAB_H
---
+++ include/impl/ELFSectionStrTab.hxx
@@ -0,0 +1,51 @@
+#ifndef ELF_SECTION_STR_TAB_HXX
+#define ELF_SECTION_STR_TAB_HXX
+
+#include "utils/helper.h"
+#include "utils/raw_ostream.h"
+
+#include <llvm/ADT/OwningPtr.h>
+#include <llvm/Support/Format.h>
+#include <llvm/Support/raw_ostream.h>
+
+template <unsigned Bitwidth>
+template <typename Archiver>
+ELFSectionStrTab<Bitwidth> *
+ELFSectionStrTab<Bitwidth>::read(Archiver &AR,
+                                 ELFSectionHeader<Bitwidth> const *sh) {
+
+  llvm::OwningPtr<ELFSectionStrTab> st(new ELFSectionStrTab());
+  st->buf.resize(sh->getSize());
+
+  // Save section_header
+  st->section_header = sh;
+
+  AR.seek(sh->getOffset(), true);
+  AR.prologue(sh->getSize());
+  AR.read_bytes(&*st->buf.begin(), sh->getSize());
+  AR.epilogue(sh->getSize());
+
+  if (!AR) {
+    // Unable to read the string table.
+    return 0;
+  }
+
+  return st.take();
+}
+
+template <unsigned Bitwidth>
+void ELFSectionStrTab<Bitwidth>::print() const {
+  using namespace llvm;
+
+  out() << '\n' << fillformat('=', 79) << '\n';
+  out().changeColor(raw_ostream::WHITE, true);
+  out() << "ELF String Table: " << this->section_header->getName() << '\n';
+  out().resetColor();
+  out() << fillformat('-', 79) << '\n';
+
+  dump_hex((unsigned char const *)&*buf.begin(), buf.size(), 0, buf.size());
+
+  out() << fillformat('=', 79) << '\n';
+}
+
+#endif // ELF_SECTION_STR_TAB_HXX
