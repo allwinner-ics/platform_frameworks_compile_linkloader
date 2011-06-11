80085915e5918ad921b30e3fd4efe2154ee4ae49
Logan Chien <tzuhsiang.chien@gmail.com>
2011-06-11 16:55:49 +0800

    Split ELFSectionSymTab into declaration and implementation.

--- include/ELFSectionSymTab.h
+++ include/ELFSectionSymTab.h
@@ -53,63 +53,6 @@ public:
 
 };
 
-
-//==================Inline Member Function Definition==========================
-
-#include "ELFSectionHeader.h"
-#include "ELFSymbol.h"
-
-template <unsigned Bitwidth>
-ELFSectionSymTab<Bitwidth>::~ELFSectionSymTab() {
-  for (size_t i = 0; i < table.size(); ++i) {
-    delete table[i];
-  }
-}
-
-template <unsigned Bitwidth>
-template <typename Archiver>
-ELFSectionSymTab<Bitwidth> *
-ELFSectionSymTab<Bitwidth>::read(Archiver &AR,
-                                 ELFObject<Bitwidth> *owner,
-                                 ELFSectionHeader<Bitwidth> const *sh) {
-
-  llvm::OwningPtr<ELFSectionSymTab<Bitwidth> > st(
-    new ELFSectionSymTab<Bitwidth>());
-
-  // Assert that entry size will be the same as standard.
-  assert(sh->getEntrySize() == TypeTraits<ELFSymbol<Bitwidth> >::size);
-
-  // Seek to the start of symbol table
-  AR.seek(sh->getOffset(), true);
-
-  // Read all symbol table entry
-  size_t size = sh->getSize() / sh->getEntrySize();
-  for (size_t i = 0; i < size; ++i) {
-    st->table.push_back(ELFSymbol<Bitwidth>::read(AR, owner, i));
-  }
-
-  if (!AR) {
-    // Unable to read the table.
-    return 0;
-  }
-
-  return st.take();
-}
-
-template <unsigned Bitwidth>
-void ELFSectionSymTab<Bitwidth>::print() const {
-  using namespace llvm;
-
-  out() << '\n' << fillformat('=', 79) << '\n';
-  out().changeColor(raw_ostream::WHITE, true);
-  out() << "Symbol Table" << '\n';
-  out().resetColor();
-
-  for (size_t i = 0; i < table.size(); ++i) {
-    table[i]->print();
-  }
-
-  out() << fillformat('=', 79) << '\n';
-}
+#include "impl/ELFSectionSymTab.hxx"
 
 #endif // ELF_SECTION_SYMTAB_H
