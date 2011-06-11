660637b0aaa5092d14df120feec0ae4cf56e4244
Logan Chien <tzuhsiang.chien@gmail.com>
2011-06-11 18:00:18 +0800

    Generate test cases for nvidia tegra2.

--- tests/images/gen-test-elf.sh
+++ tests/images/gen-test-elf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+clang -integrated-as -std=c89 -c test.c -o test-tegra2.o -ccc-host-triple armv7-none-linux-gnueabi -mcpu=cortex-a9 -mfpu=vfp
 clang -integrated-as -std=c89 -c test.c -o test-arm.o -ccc-host-triple armv7-none-linux-gnueabi
 clang -integrated-as -std=c89 -c test.c -o test-x86_32.o -ccc-host-triple i686-none-linux
 clang -integrated-as -std=c89 -c test.c -o test-x86_64.o -ccc-host-triple x86_64-none-linux
