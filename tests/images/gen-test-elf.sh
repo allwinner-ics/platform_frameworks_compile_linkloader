86196e389f2616e218de17920b6fe23d24f67786
Logan Chien <tzuhsiang.chien@gmail.com>
2011-07-02 03:12:15 +0800

    Add rodata test case.

--- tests/images/gen-test-elf.sh
+++ tests/images/gen-test-elf.sh
@@ -24,3 +24,8 @@ clang -integrated-as -std=c89 -c test.c -o test-x86_64.o -ccc-host-triple x86_64
 clang -integrated-as -std=c89 -c simple-test.c -o simple-test-arm.o -ccc-host-triple armv7-none-linux-gnueabi
 clang -integrated-as -std=c89 -c simple-test.c -o simple-test-x86_32.o -ccc-host-triple i686-none-linux
 clang -integrated-as -std=c89 -c simple-test.c -o simple-test-x86_64.o -ccc-host-triple x86_64-none-linux
+
+clang -integrated-as -std=c89 -c rodata-test.c -o rodata-test-tegra2.o -ccc-host-triple armv7-none-linux-gnueabi -mcpu=cortex-a9 -mfpu=vfp
+clang -integrated-as -std=c89 -c rodata-test.c -o rodata-test-arm.o -ccc-host-triple armv7-none-linux-gnueabi
+clang -integrated-as -std=c89 -c rodata-test.c -o rodata-test-x86_32.o -ccc-host-triple i686-none-linux
+clang -integrated-as -std=c89 -c rodata-test.c -o rodata-test-x86_64.o -ccc-host-triple x86_64-none-linux
